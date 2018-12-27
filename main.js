18ac987bdc13a75c1ff8d63806b889322fde446b
Luis Suarez <suarezluis@gmail.com>
2018-12-27 13:58:16 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-array = document.getElementsByTagName("input")
+let array = document.getElementsByTagName("input")
 for (let i = 0; i < array.length; i++) {
   if (array[i].getAttribute("type") == "password") {
     array[i].setAttribute("type", "text");
